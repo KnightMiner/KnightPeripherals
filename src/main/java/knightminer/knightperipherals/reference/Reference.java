9718f6089f5b07a86bb74ffad00897694ed432b0
KnightMiner <kirbybat4774@gmail.com>
2016-01-31 17:46:44 -0600

    Added support for loading the version from build.gradle
    Also added project URL and dependencies

--- src/main/java/knightminer/knightperipherals/reference/Reference.java
+++ src/main/java/knightminer/knightperipherals/reference/Reference.java
@@ -2,10 +2,10 @@ package knightminer.knightperipherals.reference;
 
 public class Reference {
 	
-    public static final String MOD_ID = "knightperipherals";
-    public static final String MOD_NAME = "KnightPeripherals";
-    public static final String VERSION = "1.0";
-    public static final String RESOURCE_LOCATION = MOD_ID;
+	public static final String MOD_ID = "knightperipherals";
+	public static final String MOD_NAME = "KnightPeripherals";
+	public static final String VERSION = "${version}";
+	public static final String RESOURCE_LOCATION = MOD_ID;
 
 	public static final int UPGRADE_CLAW = 190;
 	public static final int UPGRADE_HAMMER = 191;
