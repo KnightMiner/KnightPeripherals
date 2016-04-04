a9361a94c125df9b52beb9692d75b7895098705b
KnightMiner <kirbybat4774@gmail.com>
2016-04-04 15:11:01 -0500

    Fix config file
    I forgot the "all in one" method flips the parameters around

--- src/main/java/knightminer/knightperipherals/reference/Config.java
+++ src/main/java/knightminer/knightperipherals/reference/Config.java
@@ -17,27 +17,27 @@ public class Config {
 		
 		config.load();
 		
-		enableTurtleClaw = config.getBoolean("turtle_claw", "enable", true, "Enables the clicking turtle");
-		craftTurtleClaw = config.getBoolean("turtle_claw", "craft", true, "Allows the clicking turtle and turtle claw to be crafted");
-		clawFuelCost = config.getInt("turtle_claw", "fuelCost", 0, 0, 100, "Fuel cost for clicking");
+		enableTurtleClaw = config.getBoolean("enable", "turtle_claw", true, "Enables the clicking turtle");
+		craftTurtleClaw = config.getBoolean("craft", "turtle_claw", true, "Allows the clicking turtle and turtle claw to be crafted");
+		clawFuelCost = config.getInt("fuelCost", "turtle_claw", 0, 0, 100, "Fuel cost for clicking");
 
-		enableTurtleHammer = config.getBoolean("turtle_hammer", "enable", true, "Enables the smashing turtle (requires Ex Nihilo)");
-		craftTurtleHammer = config.getBoolean("turtle_hammer", "craft", true, "Allows the smashing turtle to be crafted");
+		enableTurtleHammer = config.getBoolean("enable", "turtle_hammer", true, "Enables the smashing turtle (requires Ex Nihilo)");
+		craftTurtleHammer = config.getBoolean("craft", "turtle_hammer", true, "Allows the smashing turtle to be crafted");
 
-		enableTurtleCompressedHammer = config.getBoolean("turtle_compressed_hammer", "enable", true, "Enables the crushing turtle (requires Ex Compressum)");
-		craftTurtleCompressedHammer = config.getBoolean("turtle_compressed_hammer", "craft", true, "Allows the crushing turtle to be crafted");
+		enableTurtleCompressedHammer = config.getBoolean("enable", "turtle_compressed_hammer", true, "Enables the crushing turtle (requires Ex Compressum)");
+		craftTurtleCompressedHammer = config.getBoolean("craft", "turtle_compressed_hammer", true, "Allows the crushing turtle to be crafted");
 		
-		enableTurtleBow = config.getBoolean("turtle_bow", "enable", true, "Enables the ranged turtle");
-		craftTurtleBow = config.getBoolean("turtle_bow", "craft", true, "Allows the ranged turtle to be crafted");
-		arrowsRequired = config.getBoolean("turtle_bow", "requireArrows", true, "Are arrows required for the turtle to shoot arrows");
+		enableTurtleBow = config.getBoolean("enable", "turtle_bow", true, "Enables the ranged turtle");
+		craftTurtleBow = config.getBoolean("craft", "turtle_bow", true, "Allows the ranged turtle to be crafted");
+		arrowsRequired = config.getBoolean("requireArrows", "turtle_bow", true, "Are arrows required for the turtle to shoot arrows");
 		
-		enableTurtleTnt = config.getBoolean("turtle_tnt", "enable", true, "Enables the explosive turtle");
-		craftTurtleTnt = config.getBoolean("turtle_tnt", "craft", true, "Allows the explosive turtle to be crafted");
-		tntPower = config.getInt("turtle_tnt", "explosionPower", 5, 1, 10, "Explosion power when the turtle self desctucts");
+		enableTurtleTnt = config.getBoolean("enable", "turtle_tnt", true, "Enables the explosive turtle");
+		craftTurtleTnt = config.getBoolean("craft", "turtle_tnt", true, "Allows the explosive turtle to be crafted");
+		tntPower = config.getInt("explosionPower", "turtle_tnt", 5, 1, 10, "Explosion power when the turtle self desctucts");
 		
-		enableTurtleLaser = config.getBoolean("turtle_laser", "enable", true, "Enables the mining laser turtle");
-		craftTurtleLaser = config.getBoolean("turtle_laser", "craft", true, "Allows the mining laser turtle to be crafted");
-		laserRange = config.getInt("turtle_laser", "explosionPower", 5, 1, 16, "Maximum break range for the mining laser");
+		enableTurtleLaser = config.getBoolean("enable", "turtle_laser", true, "Enables the mining laser turtle");
+		craftTurtleLaser = config.getBoolean("craft", "turtle_laser", true, "Allows the mining laser turtle to be crafted");
+		laserRange = config.getInt("range", "turtle_laser", 8, 1, 16, "Maximum break range for the mining laser");
 		
 		if (config.hasChanged())
 			config.save();
