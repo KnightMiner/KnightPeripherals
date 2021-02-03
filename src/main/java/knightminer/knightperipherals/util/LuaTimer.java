cfa11b08c6217137e1c2a70529ff99918fd5ae6b
BeefaloKing <9630438+BeefaloKing@users.noreply.github.com>
2021-02-02 19:16:04 -0700

    Log exception thrown by CC when timer fires with detached peripheral

--- src/main/java/knightminer/knightperipherals/util/LuaTimer.java
+++ src/main/java/knightminer/knightperipherals/util/LuaTimer.java
@@ -10,6 +10,7 @@ import cpw.mods.fml.common.gameevent.TickEvent.Phase;
 import cpw.mods.fml.common.gameevent.TickEvent.ServerTickEvent;
 import dan200.computercraft.api.lua.ILuaContext;
 import dan200.computercraft.api.peripheral.IComputerAccess;
+import knightminer.knightperipherals.KnightPeripherals;
 
 public class LuaTimer {
 	private static final String TIMER_EVENT = "computer_timer";
@@ -40,7 +41,8 @@ public class LuaTimer {
 						timer.getKey().queueEvent(TIMER_EVENT, new Object[] { TIMER_TOKEN + data[1] });
 					}
 					catch (RuntimeException e) { // Peripheral detached after timer started
-						continue; // Ignore exception
+						KnightPeripherals.logger.warn("LuaTimer expired after peripheral detached. CC Exception:" + e.getMessage());
+						continue;
 					}
 					iterator.remove();
 				}
